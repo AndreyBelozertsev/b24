e68b79026636700962ab4cbb93dc67c075431f49
AndreyBelozertsev <andrey.belozertsev@gmail.com>
2024-07-17 21:43:46 +0300

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <script src="//api.bitrix24.com/api/v1/"></script>
+    <title>Приложение</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <script>
+        BX24.ready(async function () {
+            await BX24.init(async function () {
+                window.axios.defaults.headers.common['X-b24api-access-token'] = BX24.getAuth().access_token;
+                window.axios.defaults.headers.common['X-b24api-domain'] = BX24.getAuth().domain;
+                window.axios.defaults.headers.common['X-b24api-member-id'] = BX24.getAuth().member_id;
+            });
+        });
+    </script>
+</head>
+<body>
+<div id="app">
+    app
+</div>
+</body>
+</html>
