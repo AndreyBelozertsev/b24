44aa7c184d843163a35412d3e12712cd481b393f
AndreyBelozertsev <andrey.belozertsev@gmail.com>
2024-07-23 14:07:24 +0300

    AddCustomPage

--- app/Http/Controllers/B24Controller.php
+++ app/Http/Controllers/B24Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use X3Group\B24Api\B24Api;
 use Illuminate\Http\Request;
 use X3Group\B24Api\Http\Controllers\B24Controller as B24ControllerBase;
 
@@ -15,7 +16,17 @@ class B24Controller extends B24ControllerBase
 
     public function custom(Request $request): \Illuminate\Contracts\View\View|\Illuminate\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\Foundation\Application
     {
-        dump($request);
+        $memberId = null;
+        if ($request->has('auth') && !empty($request->get('auth')['member_id']))
+            $memberId = $request->get('auth')['member_id'];
+
+        if ($request->has('member_id') && !empty($request->get('member_id')))
+            $memberId = $request->get('member_id');
+
+        $portal = new B24Api($memberId); 
+        $data = $portal->getApi()->request('crm.contact.get', [
+            'id' => '2085',
+        ]);
         return view('b24api/custom', []);
     }
 
