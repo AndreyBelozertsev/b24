a5a1b40acffb1d3ab2a5a06dfad2a10b7fa07e38
AndreyBelozertsev <andrey.belozertsev@gmail.com>
2024-07-23 14:43:26 +0300

    AddCustomPage

--- app/Http/Controllers/B24Controller.php
+++ app/Http/Controllers/B24Controller.php
@@ -17,14 +17,23 @@ class B24Controller extends B24ControllerBase
     public function custom(Request $request): \Illuminate\Contracts\View\View|\Illuminate\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\Foundation\Application
     {
         $memberId = null;
+        $id = null;
         if ($request->has('auth') && !empty($request->get('auth')['member_id']))
             $memberId = $request->get('auth')['member_id'];
 
         if ($request->has('member_id') && !empty($request->get('member_id')))
             $memberId = $request->get('member_id');
 
+        if ($request->has('PLACEMENT_OPTIONS') && !empty($request->get('PLACEMENT_OPTIONS'))){
+            $placement_options = json_decode('{"ID":"2085"}', true);
+        }
+
+        if(isset($placement_options['ID']) && !empty($placement_options['ID'])){
+            $id = $placement_options['ID'];
+        }
+
         $portal = new B24Api($memberId); 
-        $data = $portal->getApi()->crmContact()->get(2085);
+        $data = $portal->getApi()->crmContact()->get($id);
         
         dump($data);
         return view('b24api/custom', []);
